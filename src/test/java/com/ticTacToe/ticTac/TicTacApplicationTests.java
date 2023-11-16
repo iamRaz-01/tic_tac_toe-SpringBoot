9ec76f1b63d12539c4d88b3d0baeedb81b04ab63
razak2004 <abdulrazak.nasriudeen@fssa.freshworks.com>
2023-11-17 02:05:23 +0530

    initial commit

---
+++ src/test/java/com/ticTacToe/ticTac/TicTacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticTacToe.ticTac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
